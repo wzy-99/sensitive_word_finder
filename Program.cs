315fde58a5d277c3ca5f7dc895e266375ccc4e18
wzy-99 <lhwzy99@163.com>
2021-11-06 22:40:49 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sensitive_word_finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            if (input == "Hello World")
+            {
+                Console.WriteLine("Hello wangziyi!");
+            }
+            else
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
