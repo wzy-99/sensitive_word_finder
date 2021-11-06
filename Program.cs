97c217aa4ca00f042166eac234c5f43843934242
wzy-99 <lhwzy99@163.com>
2021-11-06 23:21:06 +0800

    v2.1

--- Program.cs
+++ Program.cs
@@ -9,14 +9,26 @@ namespace sensitive_word_finder
         {
             StreamReader sr = new StreamReader("D:/Program/homework/sensitive_word_finder/input.txt");
             StreamWriter sw = new StreamWriter("D:/Program/homework/sensitive_word_finder/output.txt");
-            string input = sr.ReadLine();
+            string input = new string("");
+            // 读取每行，并用空格连接
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                input += line + ' ';
+                line = sr.ReadLine();
+            }
+            // 删除最后一个空格
+            if (input.Length - 2 > 0)
+            {
+                input = input.Remove(input.Length - 2);
+            }
             if (input == "Hello World")
             {
                 sw.WriteLine("Hello wangziyi!");
             }
             else
             {
-                sw.WriteLine(input);
+                sw.Write(input);
             }
             sw.Close();
         }
