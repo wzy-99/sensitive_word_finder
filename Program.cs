51c99c5a119a14e5951a4270de228ee3791399ed
wzy-99 <lhwzy99@163.com>
2021-11-07 11:27:17 +0800

    v1.1
    完善功能

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace sensitive_word_finder
 {
@@ -7,13 +8,59 @@ namespace sensitive_word_finder
         static void Main(string[] args)
         {
             String input = Console.ReadLine();
-            if (input == "Hello World")
+            Finder finder = new Finder("Hello World", "Hello wangziyi");
+            String output = finder.Find(input);
+            Console.WriteLine(output);
+        }
+
+        class Finder
+        {
+            String query;
+            String replacement;
+            public Finder(String query, String replacement)
             {
-                Console.WriteLine("Hello wangziyi!");
+                this.query = query;
+                this.replacement = replacement;
             }
-            else
+            public String StringFind(String input)
             {
-                Console.WriteLine(input);
+                if (input.Contains(this.query))
+                {
+                    return input.Replace(this.query, this.replacement);
+                }
+                else
+                {
+                    return input;
+                }
+            }
+            public String FileFind(StreamReader input)
+            {
+                string buffer = new string("");
+                string line = input.ReadLine();
+                // 提前文本内容
+                while (line != null)
+                {
+                    buffer += line + ' ';
+                    line = input.ReadLine();
+                }
+                // 删除最后一个空格
+                if (buffer.Length - 1 > 0)
+                {
+                    buffer = buffer.Remove(buffer.Length - 1);
+                }
+                return this.StringFind(buffer);
+            }
+            public String Find(object input)
+            {
+                // 判断输入类型
+                if (input.GetType().Name == "String")
+                {
+                    return this.StringFind((String)input);
+                }
+                else
+                {
+                    return this.FileFind((StreamReader)input);
+                }
             }
         }
     }
