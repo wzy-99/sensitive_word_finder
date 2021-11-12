88df1a0edd9dabaa5c14d7e2c312fd2b8917acc5
wzy-99 <lhwzy99@163.com>
2021-11-12 14:09:25 +0800

    v4
    使用TransformBlock进一步简化生产者消费者模型。

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks.Dataflow;
+using System.Threading.Tasks;
+
 
 namespace sensitive_word_finder
 {
@@ -7,21 +10,45 @@ namespace sensitive_word_finder
     {
         static void Main(string[] args)
         {
-            //构建了一个命令行输入,进行文本替换，最后存储到文件并且显示的类
+            //定义一个 文本处理流程
+            TransformBlock<string, string> transformBlock = new TransformBlock<string, string>((input) =>
+            {
+                return input.Replace("Hello world", "Hello wangziyi");
+            });
+            // TransformBlock 接收message，并对每个message调用委托。TransformBlock有输出，可以连接到其他Block。
+
+            //定义一个 命令行输出流程
+            ActionBlock<string> consoleBlock = new ActionBlock<string>((input) =>
+            {
+                Console.WriteLine(input);
+            });
+            // ActionBlock 接收message，并对每个message调用委托。ActionBlock没有输出，因此不能连接到其他Block。
+
+            //定义一个 文件输出流程
+            ActionBlock<string> fileBlock = new ActionBlock<string>((input) =>
+            {
+                using (StreamWriter sw = new StreamWriter("D:/Program/homework/sensitive_word_finder/output.txt", true))
+                {
+                    sw.WriteLine(input);
+                }
+            });
+
+            BroadcastBlock<string> broadcastBlock = new BroadcastBlock<string>(p => p);
+            // BroadcastBlock 提供缓冲区，最多储存一个message，当有新的message到来时，会覆盖原来的message。
+
+            // 连接数据流
+            transformBlock.LinkTo(broadcastBlock);
+            broadcastBlock.LinkTo(consoleBlock);
+            broadcastBlock.LinkTo(fileBlock);
+
+            // 命令行读取
             ConsoleReadBlock consoleReadBlock = new ConsoleReadBlock();
-            SensitiveReplaceBlock sensitiveReplaceBlock = new SensitiveReplaceBlock("Hello world", "Hello wangziyi");
-            FileWriteBlock fileWriteBlock = new FileWriteBlock("D:/Program/homework/sensitive_word_finder/output.txt");
-            ConsoleWriteBlock consoleWriteBlock = new ConsoleWriteBlock();
 
             consoleReadBlock.DataArrived += (e) =>
             {
-                sensitiveReplaceBlock.Enqueue(e);
+                transformBlock.Post(e);
             };
-            sensitiveReplaceBlock.DataArrived += (e) =>
-            {
-                consoleWriteBlock.Enqueue(e);
-                fileWriteBlock.Enqueue(e);
-            };
+
             consoleReadBlock.Start();
         }
     }
